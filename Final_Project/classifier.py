ef41235343d8854af8e31c71574f95cf35be0bc2
matabuzz <matabuzz@iu.edu>
2023-03-16 14:15:44 -0400

    Added TweetTokenizer.

--- Final_Project/classifier.py
+++ Final_Project/classifier.py
@@ -6,9 +6,16 @@ import pandas as pd
 # from sklearn.metrics import accuracy_score, classification_report
 import re
 from nltk.tokenize import word_tokenize
+from nltk.tokenize.casual import TweetTokenizer
+
+import unicodedata
+from emoji import EMOJI_DATA
+
+def is_emoji(s):
+    return s in EMOJI_DATA
 
 # %%
-def cleanTweet(tweet):
+def cleanTweet(tweet, lang):
 
     clean_tweet = tweet.lower() # Make tweet all lowercase
 
@@ -16,7 +23,7 @@ def cleanTweet(tweet):
     clean_tweet = re.sub("\"", "", clean_tweet) # Remove double-quotes
     clean_tweet = re.sub("http[://\/-zA-Z0-9_=+.$@#$%^&*()]+", '', clean_tweet) # Remove links
     
-    clean_tweet = ' '.join(word_tokenize(clean_tweet)) # Tokenise tweet
+    clean_tweet = ' '.join(word_tokenize(clean_tweet, language = lang)) # Tokenise tweet
 
     clean_tweet = re.sub("\'", "APOS", clean_tweet) # Change apostrophes to a tag
     clean_tweet = re.sub(" %", "PERC", clean_tweet) # Change percent signs to a tag
@@ -33,35 +40,43 @@ def cleanTweet(tweet):
     
     return clean_tweet
 
+def cleanTweet2(tweet):
+
+    clean_tweet = re.sub("http[://\/-zA-Z0-9_=+.$@#$%^&*()]+", '', tweet) # Remove links
+
+    clean_tweet = ' '.join(TweetTokenizer(strip_handles = True, reduce_len = True, preserve_case = False).tokenize(clean_tweet)) # Tokenise tweet
+
+    return clean_tweet
+
 # %%
 df = pd.read_json('./EXIST2023_training.json', encoding='utf8', orient = 'index')
 
 # %%
 X_train = df['tweet'].to_list()
-Y_train = ['YES' if labels.count('YES') > 3 else 'NO' if labels.count('NO') < 3 else 'UNCLEAR' for labels in df['labels_task1'].to_list()]
-X_train_new = [cleanTweet(tweet) for tweet in X_train]
+X_train_lang = ['english' if lang == 'en' else 'spanish' for lang in df['lang'].to_list()] # Change language labels to full name
+Y_train = ['YES' if labels.count('YES') > 3 else 'NO' for labels in df['labels_task1'].to_list()] # Label as sexism if 3 or more annotators label the tweet as sexism
+X_train_new = [cleanTweet(tweet, lang) for tweet, lang in zip(X_train, X_train_lang)]
+X_train_new2 = [cleanTweet2(tweet) for tweet in X_train]
 
 # %%
 # create TF-IDF vectorizer with n-grams
-#tfidf = TfidfVectorizer(ngram_range=(2, 2), analyzer='char')
+tfidf = TfidfVectorizer(ngram_range=(2, 2), analyzer='char')
 
 # fit and transform the training data
-#X_train_tfidf = tfidf.fit_transform(X_train)
+X_train_tfidf = tfidf.fit_transform(X_train)
 
 # transform the testing data using the same vectorizer
-#X_test_tfidf = tfidf.transform(X_test)
+X_test_tfidf = tfidf.transform(X_test)
 
 # %%
 # create SVM classifier and fit the training data
-#svm = SVC(kernel='rbf')
-#svm.fit(X_train_tfidf, Y_train)
+svm = SVC(kernel='rbf')
+svm.fit(X_train_tfidf, Y_train)
 
 # predict on the testing data and calculate accuracy
-#y_pred = svm.predict(X_test_tfidf)
-#accuracy = accuracy_score(Y_test, y_pred)
-#print('Accuracy:', accuracy*100)
+y_pred = svm.predict(X_test_tfidf)
+accuracy = accuracy_score(Y_test, y_pred)
+print('Accuracy:', accuracy*100)
 
 # print classification report
-#print(classification_report(Y_test, y_pred))
-
-
+print(classification_report(Y_test, y_pred))
