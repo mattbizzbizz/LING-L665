76402ecaa60394bb11bebac1c4b1804639759c43
matabuzz <matabuzz@iu.edu>
2023-03-23 09:31:21 -0400

    Final Project files.

--- Final_Project/classifier.py
+++ Final_Project/classifier.py
@@ -40,11 +40,18 @@ def cleanTweet(tweet, lang):
     
     return clean_tweet
 
-def cleanTweet2(tweet):
+def cleanTweet_TweetTokenizer(tweet):
 
     clean_tweet = re.sub("http[://\/-zA-Z0-9_=+.$@#$%^&*()]+", '', tweet) # Remove links
 
+    clean_tweet = re.sub(r'[.]', ' . ', clean_tweet)
+
+    clean_tweet = re.sub(r"([a-z])`s", "\g<1>'s", clean_tweet)
+    clean_tweet = re.sub(r'`', '', clean_tweet)
+
     clean_tweet = ' '.join(TweetTokenizer(strip_handles = True, reduce_len = True, preserve_case = False).tokenize(clean_tweet)) # Tokenise tweet
+
+    print(f'Original Tweet: {tweet}\nCleaned Tweet: {clean_tweet}\n')
 
     return clean_tweet
 
@@ -56,27 +63,27 @@ X_train = df['tweet'].to_list()
 X_train_lang = ['english' if lang == 'en' else 'spanish' for lang in df['lang'].to_list()] # Change language labels to full name
 Y_train = ['YES' if labels.count('YES') > 3 else 'NO' for labels in df['labels_task1'].to_list()] # Label as sexism if 3 or more annotators label the tweet as sexism
 X_train_new = [cleanTweet(tweet, lang) for tweet, lang in zip(X_train, X_train_lang)]
-X_train_new2 = [cleanTweet2(tweet) for tweet in X_train]
+X_train_new2 = [cleanTweet_TweetTokenizer(tweet) for tweet in X_train]
 
-# %%
-# create TF-IDF vectorizer with n-grams
-tfidf = TfidfVectorizer(ngram_range=(2, 2), analyzer='char')
-
-# fit and transform the training data
-X_train_tfidf = tfidf.fit_transform(X_train)
-
-# transform the testing data using the same vectorizer
-X_test_tfidf = tfidf.transform(X_test)
-
-# %%
-# create SVM classifier and fit the training data
-svm = SVC(kernel='rbf')
-svm.fit(X_train_tfidf, Y_train)
-
-# predict on the testing data and calculate accuracy
-y_pred = svm.predict(X_test_tfidf)
-accuracy = accuracy_score(Y_test, y_pred)
-print('Accuracy:', accuracy*100)
-
-# print classification report
-print(classification_report(Y_test, y_pred))
+## %%
+## create TF-IDF vectorizer with n-grams
+#tfidf = TfidfVectorizer(ngram_range=(2, 2), analyzer='char')
+#
+## fit and transform the training data
+#X_train_tfidf = tfidf.fit_transform(X_train)
+#
+## transform the testing data using the same vectorizer
+#X_test_tfidf = tfidf.transform(X_test)
+#
+## %%
+## create SVM classifier and fit the training data
+#svm = SVC(kernel='rbf')
+#svm.fit(X_train_tfidf, Y_train)
+#
+## predict on the testing data and calculate accuracy
+#y_pred = svm.predict(X_test_tfidf)
+#accuracy = accuracy_score(Y_test, y_pred)
+#print('Accuracy:', accuracy*100)
+#
+## print classification report
+#print(classification_report(Y_test, y_pred))
