eaa8f60f1d78634bb4c58df4527e7842f580fae2
matabuzz <matabuzz@iu.edu>
2023-04-14 16:08:56 -0400

    Added hard labels pkl file.

--- Final_Project/classifier.py
+++ Final_Project/classifier.py
@@ -91,6 +91,8 @@ def normalize(tweet):
 
 def cleanTweet(tweet):
 
+    print(f'Original Tweet: {tweet}')
+
     clean_tweet = html.unescape(tweet) # Convert html characters to unicode
     clean_tweet = unicodedata.normalize('NFKC', clean_tweet) # Normalize font
     clean_tweet = normalize(clean_tweet) # Fix weird fonts
@@ -108,7 +110,7 @@ def cleanTweet(tweet):
     clean_tweet = re.sub(r'([0-9])%', '\g<1> percent', clean_tweet) # Convert % into the word 'percent' when directly after a number
     clean_tweet = re.sub(r'([a-zA-Z])[*]+([a-z])', '\g<1>astrickidentificationtag\g<2>', clean_tweet) # Convert censoring astricks into identification tags
 
-    clean_tweet = re.sub(r'[.´`^¨~°|─­,;‘’"“”«»()\[\]{}®\$£€*#%↓ِ\u0301\u200D]', ' ', clean_tweet) # Replace special characters with a space
+    clean_tweet = re.sub(r'[.´`^¨~°|─­,;‘’"“”«»()\[\]{}®\$£€*%↓ِ\u0301\u200D]', ' ', clean_tweet) # Replace special characters with a space
 
     clean_tweet = re.sub(r'\u00A9\uFE0F', 'c', clean_tweet) # DOUBLE-CHECK THIS Replacing copyrite symbol with a 'c'
 
@@ -129,12 +131,14 @@ def cleanTweet(tweet):
     clean_tweet = re.sub(r'[<>]', '', clean_tweet) # Remove < and >
     clean_tweet = re.sub(r' -($|\s)', ' ', clean_tweet) # Remove hyphens when not connecting words or numbers
 
+    clean_tweet = re.sub(r'#[\w]+', '<HASHTAG>', clean_tweet) # Convert hashtags to <HASHTAG>
     clean_tweet = re.sub(r'usernameidentificationtag', '<USERNAME>', clean_tweet) # Convert usernames to <USERNAME>
 
     clean_tweet = re.sub(r"([a-z>]) '[\s]*s ", "\g<1>'s ", clean_tweet) # Reattach possesives
 
     clean_tweet, username_count = remove_duplicate(clean_tweet, '<USERNAME>') # Remove duplicate <USERNAME>
     clean_tweet, possesive_username_count = remove_duplicate(clean_tweet, "<USERNAME>'s") # Remove duplicate <USERNAME>'s
+    clean_tweet, hashtag_count = remove_duplicate(clean_tweet, '<HASHTAG>') # Remove duplicate <HASHTAG>
 
     clean_tweet = replace_punct(clean_tweet, '¡', '!') # Convert upside-down exclamation points to exclamation points
     clean_tweet = replace_punct(clean_tweet, '¿', '?') # Convert upside-down question marks to question marks
@@ -155,7 +159,9 @@ def cleanTweet(tweet):
     clean_tweet = re.sub(r" '()", " ", clean_tweet) # Remove separated apostrophes
     clean_tweet = re.sub(r' +', ' ', clean_tweet) # Remove double-spaces
 
-    return clean_tweet, username_count, exclamation_count, question_count, possesive_username_count
+    print(f'Clean Tweet: {clean_tweet}\n')
+
+    return clean_tweet, username_count, exclamation_count, question_count, possesive_username_count, hashtag_count
 
 #%%
 train_data = pd.read_json('./EXIST_2023_Dataset/training/EXIST2023_training.json', encoding='utf8', orient = 'index') # Open training data json file
@@ -163,13 +169,16 @@ train_golds_soft = pd.read_json('./EXIST_2023_Dataset/evaluation/golds/EXIST2023
 X_train_ID = train_data['id_EXIST'].to_list() # Create list of training file tweet IDs
 X_train_tweet = train_data['tweet'].to_list() # Create list of tweets
 X_train_lang = ['english' if lang == 'en' else 'spanish' for lang in train_data['lang'].to_list()] # Change language labels to full name
-X_train_clean_tweet, X_train_username_counts, X_train_exclamation_counts, X_train_question_counts, X_train_possesive_username_counts = zip(*[cleanTweet(tweet) for tweet in X_train_tweet]) # Clean tweets and extract features
+X_train_clean_tweet, X_train_username_counts, X_train_exclamation_counts, X_train_question_counts, X_train_possesive_username_counts, X_train_hashtag_counts = zip(*[cleanTweet(tweet) for tweet in X_train_tweet]) # Clean tweets and extract features
 Y_train_soft = [[round(softs['YES'], 2), round(softs['NO'], 2)] for softs in train_golds_soft['soft_label'].to_list()] # Round soft labels to 2 decimal places
+Y_train_hard = ['YES' if labels.count('YES') >= 3 else 'NO' for labels in train_data['labels_task1'].to_list()] # Create list of hard labels
 
 train_data_spanish = [] # List of tweet IDs and tweets for spanish
 train_data_english = [] # List of tweet IDs and tweets for english
-train_labels_spanish = [] # List of tweet IDs and soft labels for spanish
-train_labels_english = [] # List of tweet IDs and soft labels for english
+train_soft_labels_spanish = [] # List of tweet IDs and soft labels for spanish
+train_soft_labels_english = [] # List of tweet IDs and soft labels for english
+train_hard_labels_spanish = [] # List of tweet IDs and hard labels for spanish
+train_hard_labels_english = [] # List of tweet IDs and hard labels for english
 
 ## Iterate through language labels
 for i, lang in enumerate(X_train_lang):
@@ -177,12 +186,14 @@ for i, lang in enumerate(X_train_lang):
     ## Check if language is english
     if lang == 'english':
         train_data_english.append([X_train_ID[i], X_train_clean_tweet[i]]) # Add tweet ID and tweet to list
-        train_labels_english.append([X_train_ID[i], Y_train_soft[i]]) # Add tweet ID and soft label to list
+        train_soft_labels_english.append([X_train_ID[i], Y_train_soft[i]]) # Add tweet ID and soft label to list
+        train_hard_labels_english.append([X_train_ID[i], Y_train_hard[i]]) # Add tweet ID and hard label to list
 
     ## Check if language is spanish
     elif lang == 'spanish':
         train_data_spanish.append([X_train_ID[i], X_train_clean_tweet[i]]) # Add tweet ID and tweet to list
-        train_labels_spanish.append([X_train_ID[i], Y_train_soft[i]]) # Add tweet ID and soft label to list
+        train_soft_labels_spanish.append([X_train_ID[i], Y_train_soft[i]]) # Add tweet ID and soft label to list
+        train_hard_labels_spanish.append([X_train_ID[i], Y_train_hard[i]]) # Add tweet ID and hard label to list
 
     ## If language is not english or spanish return error message and close program
     else:
@@ -197,13 +208,21 @@ with open('./pkl/train_data_english.pkl', 'wb') as fd:
 with open('./pkl/train_data_spanish.pkl', 'wb') as fd:
     pickle.dump(train_data_spanish, fd)
 
-## Save training labels for english as pkl file
-with open('./pkl/train_labels_english.pkl', 'wb') as fd:
-    pickle.dump(train_labels_english, fd)
+## Save training soft labels for english as pkl file
+with open('./pkl/train_soft_labels_english.pkl', 'wb') as fd:
+    pickle.dump(train_soft_labels_english, fd)
 
-## Save training labels for spanish as pkl file
-with open('./pkl/train_labels_spanish.pkl', 'wb') as fd:
-    pickle.dump(train_labels_spanish, fd)
+## Save training soft labels for spanish as pkl file
+with open('./pkl/train_soft_labels_spanish.pkl', 'wb') as fd:
+    pickle.dump(train_soft_labels_spanish, fd)
+
+## Save training hard labels for english as pkl file
+with open('./pkl/train_hard_labels_english.pkl', 'wb') as fd:
+    pickle.dump(train_hard_labels_english, fd)
+
+## Save training hard labels for spanish as pkl file
+with open('./pkl/train_hard_labels_spanish.pkl', 'wb') as fd:
+    pickle.dump(train_hard_labels_spanish, fd)
 
 #%%
 dev_data = pd.read_json('./EXIST_2023_Dataset/dev/EXIST2023_dev.json', encoding='utf8', orient = 'index') # Open dev data json file
@@ -211,13 +230,16 @@ dev_golds_soft = pd.read_json('./EXIST_2023_Dataset/evaluation/golds/EXIST2023_d
 X_dev_ID = dev_data['id_EXIST'].to_list() # Create list of dev file tweet IDs
 X_dev_tweet = dev_data['tweet'].to_list() # Create list of tweets
 X_dev_lang = ['english' if lang == 'en' else 'spanish' for lang in dev_data['lang'].to_list()] # Change language labels to full name
-X_dev_clean_tweet, X_dev_username_counts, X_dev_exclamation_counts, X_dev_question_counts, X_dev_possesive_username_counts = zip(*[cleanTweet(tweet) for tweet in X_dev_tweet]) # Clean tweets and extract features
+X_dev_clean_tweet, X_dev_username_counts, X_dev_exclamation_counts, X_dev_question_counts, X_dev_possesive_username_counts, X_dev_hashtag_counts = zip(*[cleanTweet(tweet) for tweet in X_dev_tweet]) # Clean tweets and extract features
 Y_dev_soft = [[round(softs['YES'], 2), round(softs['NO'], 2)] for softs in dev_golds_soft['soft_label'].to_list()] # Round soft labels to 2 decimal places
+Y_dev_hard = ['YES' if labels.count('YES') >= 3 else 'NO' for labels in dev_data['labels_task1'].to_list()] # Create list of hard labels
 
 dev_data_spanish = [] # List of tweet IDs and tweets for spanish
 dev_data_english = [] # List of tweet IDs and tweets for english
-dev_labels_spanish = [] # List of tweet IDs and soft labels for spanish
-dev_labels_english = [] # List of tweet IDs and soft labels for english
+dev_soft_labels_spanish = [] # List of tweet IDs and soft labels for spanish
+dev_soft_labels_english = [] # List of tweet IDs and soft labels for english
+dev_hard_labels_spanish = [] # List of tweet IDs and hard labels for spanish
+dev_hard_labels_english = [] # List of tweet IDs and hard labels for english
 
 ## Iterate through language labels
 for i, lang in enumerate(X_dev_lang):
@@ -225,12 +247,14 @@ for i, lang in enumerate(X_dev_lang):
     ## Check if language is english
     if lang == 'english':
         dev_data_english.append([X_dev_ID[i], X_dev_clean_tweet[i]]) # Add tweet ID and tweet to list
-        dev_labels_english.append([X_dev_ID[i], Y_dev_soft[i]]) # Add tweet ID and soft label to list
+        dev_soft_labels_english.append([X_dev_ID[i], Y_dev_soft[i]]) # Add tweet ID and soft label to list
+        dev_hard_labels_english.append([X_dev_ID[i], Y_dev_hard[i]]) # Add tweet ID and hard label to list
 
     ## Check if language is spanish
     elif lang == 'spanish':
         dev_data_spanish.append([X_dev_ID[i], X_dev_clean_tweet[i]]) # Add tweet ID and tweet to list
-        dev_labels_spanish.append([X_dev_ID[i], Y_dev_soft[i]]) # Add tweet ID and soft label to list
+        dev_soft_labels_spanish.append([X_dev_ID[i], Y_dev_soft[i]]) # Add tweet ID and soft label to list
+        dev_hard_labels_spanish.append([X_dev_ID[i], Y_dev_hard[i]]) # Add tweet ID and hard label to list
 
     ## If language is not english or spanish return error message and close program
     else:
@@ -245,20 +269,28 @@ with open('./pkl/dev_data_english.pkl', 'wb') as fd:
 with open('./pkl/dev_data_spanish.pkl', 'wb') as fd:
     pickle.dump(dev_data_spanish, fd)
 
-## Save dev labels for english as pkl file
-with open('./pkl/dev_labels_english.pkl', 'wb') as fd:
-    pickle.dump(dev_labels_english, fd)
+## Save dev soft labels for english as pkl file
+with open('./pkl/dev_soft_labels_english.pkl', 'wb') as fd:
+    pickle.dump(dev_soft_labels_english, fd)
 
-## Save dev labels for spanish as pkl file
-with open('./pkl/dev_labels_spanish.pkl', 'wb') as fd:
-    pickle.dump(dev_labels_spanish, fd)
+## Save dev soft labels for spanish as pkl file
+with open('./pkl/dev_soft_labels_spanish.pkl', 'wb') as fd:
+    pickle.dump(dev_soft_labels_spanish, fd)
+
+## Save dev hard labels for english as pkl file
+with open('./pkl/dev_hard_labels_english.pkl', 'wb') as fd:
+    pickle.dump(dev_hard_labels_english, fd)
+
+## Save dev hard labels for spanish as pkl file
+with open('./pkl/dev_hard_labels_spanish.pkl', 'wb') as fd:
+    pickle.dump(dev_hard_labels_spanish, fd)
 
 #%%
 test_data = pd.read_json('./EXIST_2023_Dataset/test/EXIST2023_test_clean.json', encoding='utf8', orient = 'index') # Open test data json file
 X_test_ID = test_data['id_EXIST'].to_list() # Create list of test file tweet IDs
 X_test_tweet = test_data['tweet'].to_list() # Create list of tweets
 X_test_lang = ['english' if lang == 'en' else 'spanish' for lang in test_data['lang'].to_list()] # Change language labels to full name
-X_test_clean_tweet, X_test_username_counts, X_test_exclamation_counts, X_test_question_counts, X_test_possesive_username_counts = zip(*[cleanTweet(tweet) for tweet in X_test_tweet]) # Clean tweets and extract features
+X_test_clean_tweet, X_test_username_counts, X_test_exclamation_counts, X_test_question_counts, X_test_possesive_username_counts, X_dev_hashtag_counts = zip(*[cleanTweet(tweet) for tweet in X_test_tweet]) # Clean tweets and extract features
 
 test_data_spanish = [] # List of tweet IDs and tweets for spanish
 test_data_english = [] # List of tweet IDs and tweets for english
